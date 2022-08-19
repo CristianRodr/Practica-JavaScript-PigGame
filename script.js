7c2c42c6316a68dcbed2f9291e786d173b622577
Cristian Rodriguez <an1me@hotmail.es>
2022-08-18 21:54:33 -0500

    script.js: Mantener la puntuación actual

--- script.js
+++ script.js
@@ -20,27 +20,54 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //funcionalidad de los dados rodantes
 btnRoll.addEventListener('click', function () {
-  // 1. Generando una tirada de dados aleatoria
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  // 2. mostramos los dados
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  //3. Buscamos una tirada a uno basicamente: if true, cambio de jugador
-  if (dice != 1) {
-    // Agregar los dados a la puntuacion actual
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Cambiar a la siguiente jugadora
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+  if(playing) {
+    // 1. Generando una tirada de dados aleatoria
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    // 2. mostramos los dados
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    //3. Buscamos una tirada a uno basicamente: if true, cambio de jugador
+    if (dice != 1) {
+      // Agregar los dados a la puntuacion actual
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Cambiar a la siguiente jugadora
+      switchPlayer();
+    }
   }
+});
+
+btnhold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Agregue la puntuacion actual a la puntuacion del jugador activo
+      scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        scores[activePlayer]
+      // 2. comprobar si la puntuación del jugador >= 100
+    if (scores[activePlayer] >= 20) {
+      // terminar el juego
+      playing = false;
+      diceEl.classList.add('hidden');
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    } else {
+      // Cambiar al siguiente jugador
+      switchPlayer();
+    }
+  }  
 });
